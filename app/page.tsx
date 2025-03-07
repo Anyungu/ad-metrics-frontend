8bf5fe4b751038180af21e129288ee746b4484ea
Anyungu Wanyungu <anyungucw@gmail.com>
2025-03-07 11:26:14 +0300

    flex chart

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
 
         {/* Charts */}
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-          <Card className="col-span-4">
+          <Card className="col-span-7">
             <CardHeader>
               <CardTitle>Ads Trend</CardTitle>
             </CardHeader>
